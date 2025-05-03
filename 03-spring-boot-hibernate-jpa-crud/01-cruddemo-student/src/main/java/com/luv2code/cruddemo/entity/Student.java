c6127df3afaccb20ca2fd53ce33416bda1142b77
rismanobahar <rismanobahar@gmail.com>
2025-05-03 17:03:26 +0700

    05/03/2025

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -0,0 +1,20 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+    //define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+
+    //define constructors
+    //define getters/setters
+}
